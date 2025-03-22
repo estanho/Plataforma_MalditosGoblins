0f70012b914a0918727a6c98e5b1fdf9c4d8d6b1
estanho <estanhoeu@gmail.com>
2025-03-21 21:05:08 -0300

    build: create setup

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,22 @@
+import js from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: globals.node },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  eslintPluginPrettierRecommended,
+]);
---
+++ frontend/src/data/data.ts
@@ -0,0 +1,58 @@
+const name = [
+  ["Plaft", "Plin", "Tik", "Tok", "Bash", "Cricri"],
+  ["Glub", "Tim", "Ranço", "Yhaa", "Vrum", "Aiaiai"],
+  ["Crash", "Zzzz", "Sussa", "Bibi", "Boom", "Bum"],
+  ["Spray", "Cringe", "Sopa", "Ovo", "Ban", "Nhack"],
+  ["Bing", "Riso", "Slash", "Coff", "Ugh", "Sniff"],
+  [], // 1,2,3=[última coisa que comeu] 4,5,6=[inverta seu nome]
+];
+const occupationAndDescriptor = {
+  occupation: [
+    "Mercenário",
+    "Caçador",
+    "Gatuno",
+    "Líder",
+    "Incendiário",
+    "Bruxo",
+  ],
+  descriptor: [
+    "Covarde",
+    "Atrapalhado",
+    "Tapado",
+    "Fracote",
+    "Medíocre",
+    "Supimpa",
+  ],
+};
+
+const characteristics = [
+  [
+    "Bomba-relógio",
+    "Minicabeça",
+    "Apêndice extra",
+    "Poros fedidos",
+    "Verdura",
+    "Role 2 vezes",
+  ],
+  ["Cinzento", "Cabeção", "Orelha extra", "Pintas", "Minion", "Fosforescente"],
+  ["Amaldiçoado", "Linguão", "Nariz extra", "Listras", "Galináceo", "Colorido"],
+  [
+    "Tom bélico",
+    "Olho gigante",
+    "Olhos extras",
+    "Pompom",
+    "Peixoso",
+    "Amarelo",
+  ],
+  ["Flutulência", "Pés gigantes", "Braço extra", "Chifre", "Felino", "Azul"],
+  [
+    "Role 2 vezes",
+    "Mão gigante",
+    "Cabeça extra",
+    "Cicatrizes",
+    "Aracnídeo",
+    "Vermelho",
+  ],
+];
+
+export default { name, occupationAndDescriptor, characteristics };
